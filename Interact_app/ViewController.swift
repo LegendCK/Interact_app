e3d190082fccfc624638fbc33f3fa809d262cf1b
LegendCK <chiragkhairnar12@gmail.com>
2025-11-06 20:50:46 +0530

    Initial Commit

---
+++ Interact_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Interact_app
+//
+//  Created by admin56 on 06/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
